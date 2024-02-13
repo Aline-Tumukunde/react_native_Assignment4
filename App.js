0f6fff275c40647777149679824433d1a1d22cb6
Aline-Tumukunde <tumukunde250@gmail.com>
2024-02-13 12:35:35 +0200

    Second commit

--- App.js
+++ App.js
@@ -5,21 +5,38 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Ionicons from "@expo/vector-icons/Ionicons";
 import HomeScreen from './screens/HomeScreen';
-import CalculatorScreen from './screens/CalculatorScreen'; // Corrected import
+import CalculatorScreen from './screens/CalculatorScreen';
 import ContactScreen from './screens/ContactScreen';
 import ProfileScreen from './screens/Profile';
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
+const Profile = ({ focused, color, size }) => (
+  <Ionicons name='person' size={size + 5} color={focused ? 'blue' : 'gray'} />
+);
+
+const H = ({ focused, color, size }) => (
+  <Ionicons name='home' size={size + 5} color={focused ? 'blue' : 'gray'} />
+);
+
+const Calculator = ({ focused, color, size }) => (
+  <Ionicons name='calculator' size={size + 5} color={focused ? 'blue' : 'gray'} />
+);
+
+const Contact = ({ focused, color, size }) => (
+  <Ionicons name='person' size={size + 5} color={focused ? 'blue' : 'gray'} />
+);
+
+
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={TabNavigator} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
-        <Drawer.Screen name="Calculator" component={CalculatorScreen} />
-        <Drawer.Screen name="Contact" component={ContactScreen} />
+        <Drawer.Screen name="Home" component={TabNavigator} options={{ drawerIcon: H }} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} options={{ drawerIcon: Profile }} />
+        <Drawer.Screen name="Calculator" component={CalculatorScreen} options={{ drawerIcon: Calculator }} />
+        <Drawer.Screen name="Contacts" component={ContactScreen} options={{ drawerIcon: Contact }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -29,7 +46,7 @@ const TabNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name='HomeScreen'
+        name='Home'
         component={HomeScreen}
         options={{
           tabBarLabel: "Home",
